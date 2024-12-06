a18a36becb8fbc864aa9d4469bb3bd5c75ab21e0
crockdaly <baninmatvey333@gmail.com>
2024-12-06 12:46:40 +1000

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Cli.cli();
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                0 - Exit
+                """);
+        String  number = scanner.nextLine();
+        System.out.println("Your choice: " + number);
+
+        if (number.equals("1")) {
+            Cli.cli();
+        } else if (number.equals("2")) {
+            Cli.cli();
+            Even even = new Even();
+            even.StartGame(scanner, Cli.getName() );
+        }
+        scanner.close();
     }
 }
+
