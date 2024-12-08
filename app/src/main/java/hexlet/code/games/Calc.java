7300952054e5a5c8430c040d3f4a8b7855675558
crockdaly <baninmatvey333@gmail.com>
2024-12-08 15:26:15 +1000

    update Calc.java game

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,32 +3,32 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import java.util.Random;
 
-public class Calc implements Engine.GameLogic {
-    private static final Random RANDOM = new Random();
-    private static final String[] OPERATORS = {"+", "-", "*"};
+    public class Calc implements Engine.GameLogic {
+        private static final Random RANDOM = new Random();
+        private static final String[] OPERATORS = {"+", "-", "*"};
 
-    @Override
-    public boolean playGround() {
-        int num1 = RANDOM.nextInt(20) + 1;
-        int num2 = RANDOM.nextInt(20) + 1;
-        String operator = OPERATORS[RANDOM.nextInt(OPERATORS.length)];
+        @Override
+        public boolean playGround() {
+            int num1 = RANDOM.nextInt(20) + 1;
+            int num2 = RANDOM.nextInt(20) + 1;
+            String operator = OPERATORS[RANDOM.nextInt(OPERATORS.length)];
 
-        int correctAnswer = switch (operator) {
-            case "+" -> num1 + num2;
-            case "-" -> num1 - num2;
-            case "*" -> num1 * num2;
-            default -> throw new IllegalStateException("Unexpected value: " + operator);
-        };
+            int correctAnswer = switch (operator) {
+                case "+" -> num1 + num2;
+                case "-" -> num1 - num2;
+                case "*" -> num1 * num2;
+                default -> throw new IllegalStateException("Unexpected value: " + operator);
+            };
 
-        Engine.printMessage("Question: " + num1 + " " + operator + " " + num2);
-        String answer = Engine.readInput("Your answer: ");
+            Engine.printMessage("Question: " + num1 + " " + operator + " " + num2);
+            String answer = Engine.readInput("Your answer: ");
 
-        if (!answer.equals(String.valueOf(correctAnswer))) {
-            Engine.printMessage("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
-            return false;
+            if (!answer.equals(String.valueOf(correctAnswer))) {
+                Engine.printMessage("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+                return false;
+            }
+
+            Engine.printMessage("Correct!");
+            return true;
         }
-
-        Engine.printMessage("Correct!");
-        return true;
     }
-}
