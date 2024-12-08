74d15fc4c6ffd2e0606bcb5071919d6d8e47bedf
crockdaly <baninmatvey333@gmail.com>
2024-12-08 15:27:46 +1000

    add GCP find logic

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -47,5 +47,13 @@ import java.util.Scanner;
             }
             printMessage("Congratulations, " + userName + "!");
         }
+        public static int findGDC(int num1, int num2) {
+            while (num2 != 0) {
+                int temp = num2;
+                num2 = num1 % num2;
+                num1 = temp;
+            }
+            return num1;
+        }
 
     }
